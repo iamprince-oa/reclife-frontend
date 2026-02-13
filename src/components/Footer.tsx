292cebc283e11f7b29681a0ce4b98c7f290fde14
Prince Owusu Asante <owusup484@gmail.com>
2026-02-13 03:28:50 +0000

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/footer.css";
 
 function Footer() {
@@ -7,29 +8,36 @@ function Footer() {
     <footer className="footer">
       <div className="footer-container">
         {/* Contact Section */}
-        <div className="footer-section contact">
-          <h3>Get In Touch</h3>
-          <p>6033 Shawson Drive, Mississauga, ON L5T 1H8</p>
+        <div className="footer-section footer-contact">
+          <h4>Get In Touch</h4>
           <p>
+            <i className="bi bi-geo-alt-fill"></i> 6033 Shawson Drive,
+            Mississauga, ON L5T 1H8
+          </p>
+          <p>
+            <i className="bi bi-envelope-fill"></i>{" "}
             <a href="mailto:reclifegma@gmail.com">reclifegma@gmail.com</a>
           </p>
           <p>
+            <i className="bi bi-telephone-fill"></i>{" "}
             <a href="tel:+16476770275">647-677-0275</a>
           </p>
         </div>
 
         {/* Social Section */}
-        <div className="footer-section social">
-          <h3>Follow Us On</h3>
-          <p>
-            <a
-              href="https://instagram.com/reclifegma"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram
-            </a>
-          </p>
+        <div className="footer-section footer-social">
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <a
+                href="https://instagram.com/reclifegma"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="bi bi-instagram"></i> Instagram
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
 
@@ -40,7 +48,13 @@ function Footer() {
         </p>
         <p>
           Deployed by{" "}
-          <a href="https://startlinkvisuals.com">Startlink Visuals</a>
+          <a
+            href="https://startlinkvisuals.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ExceedLabs
+          </a>
         </p>
       </div>
     </footer>
